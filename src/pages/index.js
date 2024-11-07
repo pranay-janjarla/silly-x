316d086331ab82fd9d84647760df44305baad57a
pranay-janjarla <pranaymessi12345@gmail.com>
2024-11-08 00:30:03 +0530

    message list

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ const HomePage = () => {
   return (
     <div className="App">
       {/* <h1>Study Aid Generator</h1> */}
+      <MessageList />
       <AskAiButton />
       {/* <MindMap />
       <TreeDiagram />
